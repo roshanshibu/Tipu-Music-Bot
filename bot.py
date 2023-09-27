054ee15c78d34109e051264563378aa9627964d8
Roshan Shibu <roshanshibu97@gmail.com>
2023-09-27 08:48:35 +0000

    prittier + gitignore

--- bot.py
+++ bot.py
@@ -15,13 +15,15 @@ import dbHelper
 import eyed3
 import config
 import sys
-#the working directory when the script is run as a service is [C:\Users\---\AppData\Local\Programs\Python\Python310]
+
+# the working directory when the script is run as a service is [C:\Users\---\AppData\Local\Programs\Python\Python310]
 
 update_id = None
 TOKEN = config.BOT_TOKEN
 TIPU_USERNAME = config.TIPU_USERNAME
 
 known_users = config.known_users
+
 
 async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
     bot = context.bot
@@ -35,9 +37,9 @@ async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
         userid = update.message.from_user.id
 
         is_group = False
-        if (update.message.chat.type in ['group', 'supergroup']):
+        if update.message.chat.type in ["group", "supergroup"]:
             is_group = True
-            if (user_msg[0] == '/'):
+            if user_msg[0] == "/":
                 user_msg = user_msg[1:]
             # in groups, the bot has to be called explicitly with @BotUsername in the message
             # otherwise the bot will try to respond to every message and url sent in the group
@@ -45,28 +47,33 @@ async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 return
             else:
                 user_msg = user_msg.replace(TIPU_USERNAME, "").strip()
-            
-        if (userid in known_users):
+
+        if userid in known_users:
             await process_message(chatid, user_msg, bot, userid, is_group)
         # don't talk to strangers
-        #else:
-            #generic_response =  "Greetings"
+        # else:
+        # generic_response =  "Greetings"
+
 
 async def process_message(chatid, message, bot, userid, is_group):
     now = datetime.now()
     logging.info("===============================================================")
-    logging.info(f"Message from [{known_users[userid]}] - \"{message}\" Group? [{str(is_group)}]")
-    if (validators.url(message)):
-        downloading_msg_details = await bot.send_message(chat_id = chatid, text = "downloading...")
+    logging.info(
+        f'Message from [{known_users[userid]}] - "{message}" Group? [{str(is_group)}]'
+    )
+    if validators.url(message):
+        downloading_msg_details = await bot.send_message(
+            chat_id=chatid, text="downloading..."
+        )
 
         mp3file, error_message = musicHelper.download_music_embed_ytdl(message)
         # if yt-dlp fails, try with pytube
-        if (mp3file == "ERROR"):
+        if mp3file == "ERROR":
             mp3file, error_message = musicHelper.download_music(message)
-        
-        if (mp3file != "ERROR"):
+
+        if mp3file != "ERROR":
             logging.info(f"Downloaded file [{mp3file}]")
-            
+
             thumbnail_file_loc = config.THUMBNAIL_PATH
             # get the album art of the mp3 file about to be sent
             try:
@@ -78,27 +85,35 @@ async def process_message(chatid, message, bot, userid, is_group):
             except:
                 logging.error("Error while getting album art for telegram chat")
 
-            logging.info(f"Starting to send MP3 file [{mp3file}]...")            
-            await bot.send_audio(chat_id = chatid, audio=open(mp3file, 'rb'), thumb=open(thumbnail_file_loc,"rb"))
+            logging.info(f"Starting to send MP3 file [{mp3file}]...")
+            await bot.send_audio(
+                chat_id=chatid,
+                audio=open(mp3file, "rb"),
+                thumb=open(thumbnail_file_loc, "rb"),
+            )
             logging.info(f"MP3 file [{mp3file}] was sent to chat!")
-            #save the file to pCloud also
+            # save the file to pCloud also
             start_pcloud_upload()
         else:
             if error_message and error_message.strip():
-                await bot.send_message(chat_id = chatid, text = error_message)
+                await bot.send_message(chat_id=chatid, text=error_message)
             else:
-                await bot.send_message(chat_id = chatid, text = "something went wrong 😔")
-        await bot.delete_message(chat_id = chatid, message_id=downloading_msg_details.message_id)
+                await bot.send_message(chat_id=chatid, text="something went wrong 😔")
+        await bot.delete_message(
+            chat_id=chatid, message_id=downloading_msg_details.message_id
+        )
     else:
-        response = "Hi "+known_users[userid]+"!"
-        await bot.send_message(chat_id = chatid, text = response)
+        response = "Hi " + known_users[userid] + "!"
+        await bot.send_message(chat_id=chatid, text=response)
 
 
 async def save_audio_file(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    logging.info(update.message.audio.file_name + " was sent to Tipu to be saved and replicated")
+    logging.info(
+        update.message.audio.file_name + " was sent to Tipu to be saved and replicated"
+    )
     save_path = config.MUSIC_DIR + update.message.audio.file_name
-    
-    #save song details to db
+
+    # save song details to db
     m_info = dbHelper.MusicInfo()
     m_info.source = "direct upload"
     m_info.filename = save_path
@@ -107,39 +122,48 @@ async def save_audio_file(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # dowload the file to local storage
     music_file = await update.message.audio.get_file()
     await music_file.download_to_drive(save_path)
-    await context.bot.send_message(chat_id = update.message.chat.id, text = update.message.audio.file_name + " was saved to disk. Replication will follow.")
-    
-    #save the file to pCloud
+    await context.bot.send_message(
+        chat_id=update.message.chat.id,
+        text=update.message.audio.file_name
+        + " was saved to disk. Replication will follow.",
+    )
+
+    # save the file to pCloud
     start_pcloud_upload()
+
 
 def start_pcloud_upload():
     logging.info("********\tStarting thread to upload file to pcloud\t********")
-    _thread.start_new_thread(os.system, ('python \"' + config.PROJECT_DIR + '\\pcloudUpload.py\"',))
+    _thread.start_new_thread(
+        os.system, ('python3 "' + config.PROJECT_DIR + '/pcloudUpload.py"',)
+    )
 
-log_format = '%(asctime)s - %(levelname)s - %(filename)15s:%(lineno)3s - %(funcName)20s() ::: %(message)s'
-log_date_format = '%d-%m-%Y %H:%M:%S'
+
+log_format = "%(asctime)s - %(levelname)s - %(filename)15s:%(lineno)3s - %(funcName)20s() ::: %(message)s"
+log_date_format = "%d-%m-%Y %H:%M:%S"
 root_logger = logging.getLogger()
 
 # Add the log message handler to the logger
 handler = logging.handlers.RotatingFileHandler(
-              config.LOG_PATH, maxBytes=120000, backupCount=5)
+    config.LOG_PATH, maxBytes=120000, backupCount=5
+)
 handler.setFormatter(logging.Formatter(log_format, log_date_format))
 
 old_stdout = sys.stdout
-sys.stdout = handler.stream 
+sys.stdout = handler.stream
 
 root_logger.addHandler(handler)
 
 root_logger.setLevel(logging.INFO)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     application = ApplicationBuilder().token(TOKEN).build()
 
     reply_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), reply)
 
     # for when audio file is sent to Tipu directly
     audio_handler = MessageHandler(filters.AUDIO & (~filters.COMMAND), save_audio_file)
-    
+
     application.add_handler(reply_handler)
     application.add_handler(audio_handler)
 
--- musicHelper.py
+++ musicHelper.py
@@ -1,4 +1,4 @@
-from pytube import YouTube,Search
+from pytube import YouTube, Search
 import os
 from urllib.parse import urlparse
 import spotipy
@@ -19,6 +19,7 @@ from pathlib import Path
 import re
 
 import logging
+
 log = logging.getLogger(__name__)
 
 MUSIC_DIR = config.MUSIC_DIR
@@ -26,9 +27,9 @@ PROJECT_DIR = config.PROJECT_DIR
 THUMBNAIL_PATH = config.THUMBNAIL_PATH
 
 errorList = {
-    "UNKNOWN_HOST_ERR" : "UNKNOWN_HOST_ERR::umm, i don't know what to do with <HOSTNAME> 😬",
-    "SPOTIFY_PLAYLIST_ERR" : "SPOTIFY_PLAYLIST_ERR::Spotify playlists are not handled 😐",
-    "YT_NO_RESULTS_ERR" : "YT_NO_RESULTS_ERR::Could not find any results for \"<SEARCHQUERY>\" on Youtube 😣"
+    "UNKNOWN_HOST_ERR": "UNKNOWN_HOST_ERR::umm, i don't know what to do with <HOSTNAME> 😬",
+    "SPOTIFY_PLAYLIST_ERR": "SPOTIFY_PLAYLIST_ERR::Spotify playlists are not handled 😐",
+    "YT_NO_RESULTS_ERR": 'YT_NO_RESULTS_ERR::Could not find any results for "<SEARCHQUERY>" on Youtube 😣',
 }
 
 SPOTIFY_PLAYLIST_URI = config.SPOTIFY_PLAYLIST_URI
@@ -36,23 +37,23 @@ SPOTIFY_PLAYLIST_URI = config.SPOTIFY_PLAYLIST_URI
 SPOTIFY_USER = config.SPOTIFY_USER
 SPOTIFY_CLIENT_ID = config.SPOTIFY_CLIENT_ID
 SPOTIFY_CLIENT_SECRET = config.SPOTIFY_CLIENT_SECRET
-scope = 'playlist-modify-private playlist-modify-public'
-    
+scope = "playlist-modify-private playlist-modify-public"
+
 
 def download_music(url):
     try:
-        logging.info (f"Working directory - [{os.getcwd()}]")
+        logging.info(f"Working directory - [{os.getcwd()}]")
         m_info = dbHelper.MusicInfo()
         logging.info(f"url [{url}]")
         m_info.url = url
 
-        #check if we have already downloaded from this url
+        # check if we have already downloaded from this url
         p_file_meta = dbHelper.is_music_present_in_db(url)
-        if(p_file_meta is not None):
+        if p_file_meta is not None:
             logging.info(f"File was already downloaded from [{url}]")
             return (p_file_meta[0], p_file_meta[1])
 
-        if (is_youtube_url(url)):
+        if is_youtube_url(url):
             yt = YouTube(url)
             m_info.source = "YouTube"
         else:
@@ -61,25 +62,25 @@ def download_music(url):
 
         logging.info("Fetching video details...")
         video = yt.streams.filter(only_audio=True).first()
-        destination = MUSIC_DIR 
+        destination = MUSIC_DIR
         logging.info("starting download...")
         out_file = video.download(output_path=destination)
         logging.info("download complete!")
         base, ext = os.path.splitext(out_file)
-        new_file = base + '.mp3'
+        new_file = base + ".mp3"
         m_info.filename = new_file
         mp4_to_mp3(out_file, new_file, True)
-        
-        set_yt_thumbnail(yt, new_file, 'music.youtube' in url)
+
+        set_yt_thumbnail(yt, new_file, "music.youtube" in url)
         duration = yt.length
 
         artist_name = yt.author
         if artist_name[-7:] == "- Topic":
             artist_name = artist_name[:-7].strip()
-        metadata ={"duration": str(duration), "artist": artist_name}
+        metadata = {"duration": str(duration), "artist": artist_name}
         metadata_json = str(json.dumps(metadata))
         m_info.metadata = metadata_json
-        
+
         dbHelper.insert_music_base_info(m_info)
         return (new_file, metadata_json)
     except FileExistsError:
@@ -88,22 +89,25 @@ def download_music(url):
     except Exception as e:
         return process_error(str(e))
 
+
 def set_yt_thumbnail(yt_object, audio_file, is_yt_music):
     try:
-        #first, download the thumbnail image
+        # first, download the thumbnail image
         logging.info(f"Youtube thumbnail available at [{yt_object.thumbnail_url}]")
         response = requests.get(yt_object.thumbnail_url)
         if response.status_code:
-            fp = open(THUMBNAIL_PATH, 'wb')
+            fp = open(THUMBNAIL_PATH, "wb")
             fp.write(response.content)
             fp.close()
-        #now, we use eyed3 to set this thumbnail for our audio file
+        # now, we use eyed3 to set this thumbnail for our audio file
         audiofile = eyed3.load(audio_file)
-        if (audiofile.tag == None):
+        if audiofile.tag == None:
             audiofile.initTag()
 
         audiofile.tag.album = config.DEFAULT_ALBUM_NAME
-        audiofile.tag.images.set(ImageFrame.FRONT_COVER, open(THUMBNAIL_PATH, 'rb').read(), 'image/jpeg')
+        audiofile.tag.images.set(
+            ImageFrame.FRONT_COVER, open(THUMBNAIL_PATH, "rb").read(), "image/jpeg"
+        )
         artist_name = yt_object.author
         if artist_name[-7:] == "- Topic":
             artist_name = artist_name[:-7].strip()
@@ -112,25 +116,29 @@ def set_yt_thumbnail(yt_object, audio_file, is_yt_music):
         audiofile.tag.save()
         logging.info(f"Thumbnail added to file [{audio_file}]")
     except Exception as e:
-        logging.error(f"Error while adding thumbnail to file [{audio_file}] | Exception {e}")
-        
+        logging.error(
+            f"Error while adding thumbnail to file [{audio_file}] | Exception {e}"
+        )
+
+
 def mp4_to_mp3(mp4, mp3, delete_source_mp4):
     logging.info("converting to mp3...")
     mp4_without_frames = AudioFileClip(mp4)
     mp4_without_frames.write_audiofile(mp3, verbose=False, logger=None)
-    mp4_without_frames.close() 
-    if (delete_source_mp4):
+    mp4_without_frames.close()
+    if delete_source_mp4:
         os.remove(mp4)
-    logging.info ("conversion complete")
+    logging.info("conversion complete")
+
 
 def process_error(error_msg):
-    logging.error (f"Error! [{error_msg}]")
+    logging.error(f"Error! [{error_msg}]")
     for err in errorList:
         if err in error_msg:
-            return ("ERROR", error_msg.replace(err+"::" ,""))
+            return ("ERROR", error_msg.replace(err + "::", ""))
     logging.error("Unexpected error: %s", error_msg)
     return ("ERROR", "")
-        
+
 
 def get_hostname(url):
     parsed_uri = urlparse(url)
@@ -138,77 +146,98 @@ def get_hostname(url):
     path = parsed_uri.path
     return (hostname, path)
 
+
 def is_youtube_url(url):
     hostname = get_hostname(url)[0]
     yt_hostnames = ["youtube", "youtu.be"]
-    for yt_hostname in (yt_hostnames):
-        if (yt_hostname in hostname):
+    for yt_hostname in yt_hostnames:
+        if yt_hostname in hostname:
             return True
     else:
         return False
 
+
 def get_yt_object(url):
     hostname = get_hostname(url)[0]
-    if ("spotify" in hostname):
+    if "spotify" in hostname:
         logging.info("Song from spotify, find it on yt")
         path = get_hostname(url)[1]
         stype = "PLAYLIST"
-        if ("track/" in path):
+        if "track/" in path:
             stype = "TRACK"
         path = path[1:]
-        uri = path[path.index('/')+1:]
+        uri = path[path.index("/") + 1 :]
         yt_obj = get_yt_object_for_spotify(url, stype, uri)
         return (yt_obj, "Spotify")
     else:
-        raise Exception(errorList['UNKNOWN_HOST_ERR'].replace('<HOSTNAME>', hostname))
-    
+        raise Exception(errorList["UNKNOWN_HOST_ERR"].replace("<HOSTNAME>", hostname))
+
+
 def get_yt_object_for_spotify(s_url, resource_type, uri):
     yt_url = s_url
-    if (resource_type == "PLAYLIST"):
+    if resource_type == "PLAYLIST":
         logging.error("Spolity playlist url was given, raising error")
-        raise Exception(errorList['SPOTIFY_PLAYLIST_ERR'])
+        raise Exception(errorList["SPOTIFY_PLAYLIST_ERR"])
 
-    token = util.prompt_for_user_token(SPOTIFY_USER,scope,SPOTIFY_CLIENT_ID,SPOTIFY_CLIENT_SECRET,"http://localhost:8080/")
+    token = util.prompt_for_user_token(
+        SPOTIFY_USER,
+        scope,
+        SPOTIFY_CLIENT_ID,
+        SPOTIFY_CLIENT_SECRET,
+        "http://localhost:8080/",
+    )
     sp = spotipy.Spotify(auth=token)
 
     track_details = sp.track(uri)
     song_name = track_details["name"]
     artists = ""
-    for artist_details in (track_details["artists"]):
+    for artist_details in track_details["artists"]:
         artists += artist_details["name"] + " "
     sq = artists + song_name
-    logging.info (f"search query = [{sq}]")
+    logging.info(f"search query = [{sq}]")
     s = Search(sq)
-    if (len(s.results) == 0):
-        raise Exception(errorList['YT_NO_RESULTS_ERR'].replace('<SEARCHQUERY>', sq))
+    if len(s.results) == 0:
+        raise Exception(errorList["YT_NO_RESULTS_ERR"].replace("<SEARCHQUERY>", sq))
 
     add_song_to_spotify_pl(sp, uri, SPOTIFY_PLAYLIST_URI)
 
-    return (s.results[0])
+    return s.results[0]
 
 
 def add_song_to_spotify_pl(spotify_ob, track_uri, playlist_uri):
     spotify_ob.user_playlist_add_tracks(
-        user=SPOTIFY_USER,
-        playlist_id=playlist_uri,
-        tracks=[track_uri],
-        position=None)
+        user=SPOTIFY_USER, playlist_id=playlist_uri, tracks=[track_uri], position=None
+    )
     return
 
 
 def download_music_ytdl(url):
     try:
         # subprocess.run(["Binaries/yt-dlp.exe", url, "--ffmpeg-location", "Binaries/ffmpeg.exe", "--extract-audio", "--audio-format", "mp3", "--no-playlist", "--write-thumbnail", "--add-metadata"])
-        proc=subprocess.Popen(["Binaries/yt-dlp.exe", url, "--ffmpeg-location", "Binaries/ffmpeg.exe", "--extract-audio", "--audio-format", "mp3", "--no-playlist", "--write-thumbnail", "--add-metadata"], 
-                              stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        proc = subprocess.Popen(
+            [
+                "Binaries/yt-dlp.exe",
+                url,
+                "--ffmpeg-location",
+                "Binaries/ffmpeg.exe",
+                "--extract-audio",
+                "--audio-format",
+                "mp3",
+                "--no-playlist",
+                "--write-thumbnail",
+                "--add-metadata",
+            ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+        )
         for line in proc.stdout:
-            if '[download]' not in str(line):
+            if "[download]" not in str(line):
                 logging.info(line)
         proc.wait()
 
         # get the latest created mp3 and webp file
-        mp3_files = glob.glob(f'{config.MUSIC_DIR}*.mp3')
-        webp_files = glob.glob(f'{config.MUSIC_DIR}*.webp')
+        mp3_files = glob.glob(f"{config.MUSIC_DIR}*.mp3")
+        webp_files = glob.glob(f"{config.MUSIC_DIR}*.webp")
         latest_mp3 = max(mp3_files, key=os.path.getctime)
         latest_webp = max(webp_files, key=os.path.getctime)
         print(latest_mp3, latest_webp)
@@ -220,10 +249,12 @@ def download_music_ytdl(url):
             album_art_img.save(latest_webp, "jpeg")
 
         audio_file = eyed3.load(latest_mp3)
-        if (audio_file.tag == None):
+        if audio_file.tag == None:
             audio_file.initTag()
 
-        audio_file.tag.images.set(ImageFrame.FRONT_COVER, open(latest_webp, 'rb').read(), 'image/jpeg')
+        audio_file.tag.images.set(
+            ImageFrame.FRONT_COVER, open(latest_webp, "rb").read(), "image/jpeg"
+        )
         audio_file.tag.save(version=eyed3.id3.ID3_V2_3)
         audio_file.tag.save()
 
@@ -232,40 +263,43 @@ def download_music_ytdl(url):
     except Exception as e:
         return process_error(str(e))
 
+
 def download_music_embed_ytdl(download_url):
     try:
-        #check if we have already downloaded from this url
+        # check if we have already downloaded from this url
         p_file_meta = dbHelper.is_music_present_in_db(download_url)
-        if(p_file_meta is not None):
+        if p_file_meta is not None:
             logging.info(f"File was already downloaded from [{download_url}]")
             return (p_file_meta[0], p_file_meta[1])
-        
+
         m_info = dbHelper.MusicInfo()
         m_info.url = download_url
         m_info.source = "YouTube"
         ydl_opts = {
-        'format': 'mp3/bestaudio/best',
-        'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        }],
-        'outtmpl': '%(title)s.%(ext)s',
-        'writethumbnail': True,
-        'noplaylist': True,
+            "format": "mp3/bestaudio/best",
+            "postprocessors": [
+                {
+                    "key": "FFmpegExtractAudio",
+                    "preferredcodec": "mp3",
+                }
+            ],
+            "outtmpl": "%(title)s.%(ext)s",
+            "writethumbnail": True,
+            "noplaylist": True,
         }
-        
+
         ydl = yt_dlp.YoutubeDL(ydl_opts)
         info_dict = ydl.extract_info(download_url, download=True)
-        
+
         uploader = info_dict.get("uploader")
         if info_dict.get("artist") is not None:
             uploader = info_dict.get("artist")
         uploader = uploader.replace(" - Topic", "")
         uploader = uploader.replace("VEVO", "")
-        
+
         filename = ydl.prepare_filename(info_dict)
-        filename_wo_ext = Path(filename).with_suffix('')
-        # thumbnails may come in any format. To get the correct file, just find the one with the 
+        filename_wo_ext = Path(filename).with_suffix("")
+        # thumbnails may come in any format. To get the correct file, just find the one with the
         # same name as the music file but with a different extension
         thumbnail = ""
         all_files = glob.glob(f"{filename_wo_ext}.*")
@@ -274,35 +308,37 @@ def download_music_embed_ytdl(download_url):
                 thumbnail = file
         filename = str(filename_wo_ext) + ".mp3"
         print(f"\n - Downloaded {filename}\n - Thumbnail {thumbnail}")
-        
-        if (filename):
+
+        if filename:
             # convert the thumbnail image type to jpeg in all cases
             thumbnail_img = Image.open(thumbnail)
             album_art_img = thumbnail_img
-            if "music.youtube.com/" in download_url:    
+            if "music.youtube.com/" in download_url:
                 # crop the thumbnail only if its from youtube music
                 album_art_img = thumbnail_img.crop((280, 0, 1000, 720))
             album_art_img.save(thumbnail, "jpeg")
-        
+
         audio_file = eyed3.load(filename)
-        if (audio_file.tag == None):
+        if audio_file.tag == None:
             audio_file.initTag()
-        
-        audio_file.tag.images.set(ImageFrame.FRONT_COVER, open(thumbnail, 'rb').read(), 'image/jpeg')
-        
-        #also set the artist name
+
+        audio_file.tag.images.set(
+            ImageFrame.FRONT_COVER, open(thumbnail, "rb").read(), "image/jpeg"
+        )
+
+        # also set the artist name
         audio_file.tag.artist = uploader
         audio_file.tag.save(version=eyed3.id3.ID3_V2_3)
         audio_file.tag.save()
         # update the thumbnail file
         os.replace(thumbnail, config.THUMBNAIL_PATH)
         # move the mp3 file to desired location
-        final_filename = config.MUSIC_DIR + re.sub("[\"\']", "", filename)
+        final_filename = config.MUSIC_DIR + re.sub("[\"']", "", filename)
         os.rename(filename, final_filename)
         filename = final_filename
         m_info.filename = filename
-        
-        metadata ={"duration": str(info_dict.get("duration")), "artist": uploader}
+
+        metadata = {"duration": str(info_dict.get("duration")), "artist": uploader}
         metadata_json = str(json.dumps(metadata))
         m_info.metadata = metadata_json
         dbHelper.insert_music_base_info(m_info)
@@ -313,7 +349,7 @@ def download_music_embed_ytdl(download_url):
     except Exception as e:
         return process_error(str(e))
 
-#mp3file = dowmload_music("https://youtu.be/lXPYzs2BgWg")
-#mp3file = dowmload_music("https://open.spotify.com/track/5JKU2tXiG3yvJtefNwe7ZQ?si=RaJ6sp2cR5Ow-7lZM7R3wA&utm_source=native-share-menu")
-#print ("File downloaded ["+mp3file+"]")
 
+# mp3file = dowmload_music("https://youtu.be/lXPYzs2BgWg")
+# mp3file = dowmload_music("https://open.spotify.com/track/5JKU2tXiG3yvJtefNwe7ZQ?si=RaJ6sp2cR5Ow-7lZM7R3wA&utm_source=native-share-menu")
+# print ("File downloaded ["+mp3file+"]")
